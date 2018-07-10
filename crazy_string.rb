6216b9dd0cf79b3c1f651a8d0d05085e90a47c05
osoyliz <lizosoy18@gmail.com>
2018-07-10 18:04:01 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,4 +1,4 @@
 # write your method here
 def crazy_strings (word_one, word_two)
-  puts "#{word_one.reverse.upcase}" "#{word_two.swapcase.gusb("s","z")
+  puts "#{word_one.reverse.upcase.gusb("s,"z"}" "#{word_two.swapcase.gusb("s","z")}"
 end

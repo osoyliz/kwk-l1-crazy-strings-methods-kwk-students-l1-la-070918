14680b0c486d575f692fc8ff2718f7c34ecc867a
osoyliz <lizosoy18@gmail.com>
2018-07-10 17:54:11 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 # write your method here
 word_one= "hello".reverse
 word_two= "friends".swapcase
-def crazy_strings ("word_one + word_two")
+def crazy_strings ("word_one", "word_two")
   puts "#{crazy_strings}"
 end
